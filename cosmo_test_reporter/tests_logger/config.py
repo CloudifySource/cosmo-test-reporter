439eeaed4704c9d7421e92bb14bb635ad22617e8
nirbir <nirb@gigaspaces.com>
2014-03-25 17:27:30 +0200

    initial commit of the tests logger

---
+++ cosmo_test_reporter/tests_logger/config.py
@@ -0,0 +1,47 @@
+__author__ = 'nirb'
+
+
+# flake8: NOQA
+# logger configuration
+from os import path
+LOG_DIR = path.expanduser('./')
+MODULE = 'cli'
+LOGGER = {
+    "version": 1,
+    "formatters": {
+        "file": {
+            "format": "%(asctime)s [%(levelname)s] %(message)s"
+        },
+        "console": {
+            "format": "%(message)s"
+        }
+    },
+    "handlers": {
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "file",
+            "level": "DEBUG",
+            "filename": "{0}/cloudify-{1}.log".format(LOG_DIR, MODULE),
+            "maxBytes": "5000000",
+            "backupCount": "20"
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "console"
+        }
+    },
+    "loggers": {
+        "main": {
+            "handlers": [
+                "console",
+                'file'
+            ]
+        },
+        "file": {
+            "handlers": [
+                'file'
+            ]
+        }
+    }
+}
