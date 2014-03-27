113a034325f5a3d244b6807dbda3a7153937f404
nirbir <nirb@gigaspaces.com>
2014-03-27 15:48:43 +0200

    refactoring of the logger config - making it dynamic

--- cosmo_test_reporter/tests_logger/config.py
+++ cosmo_test_reporter/tests_logger/config.py
@@ -4,44 +4,49 @@ __author__ = 'nirb'
 # flake8: NOQA
 # logger configuration
 from os import path
-LOG_DIR = path.expanduser('./')
-MODULE = 'test'
-LOGGER = {
-    "version": 1,
-    "formatters": {
-        "file": {
-            "format": "%(asctime)s [%(levelname)s] %(message)s"
-        },
-        "console": {
-            "format": "%(message)s"
+
+
+class LoggerConfiguration():
+
+    def __init__(self, file_name):
+        self.log_dir = path.expanduser('.')
+        self.file = file_name
+        self.logger_conf = {
+            "version": 1,
+            "formatters": {
+                "file": {
+                    "format": "%(asctime)s [%(levelname)s] %(message)s"
+                },
+                "console": {
+                    "format": "%(message)s"
+                }
+            },
+            "handlers": {
+                "file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "formatter": "file",
+                    "level": "DEBUG",
+                    "filename": "{0}/{1}.log".format(self.log_dir, self.file),
+                    "maxBytes": "5000000",
+                    "backupCount": "20"
+                },
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://sys.stdout",
+                    "formatter": "console"
+                }
+            },
+            "loggers": {
+                "main": {
+                    "handlers": [
+                        "console",
+                        'file'
+                    ]
+                },
+                "file": {
+                    "handlers": [
+                        'file'
+                    ]
+                }
+            }
         }
-    },
-    "handlers": {
-        "file": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "file",
-            "level": "DEBUG",
-            "filename": "{0}/{1}.log".format(LOG_DIR, MODULE),
-            "maxBytes": "5000000",
-            "backupCount": "20"
-        },
-        "console": {
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-            "formatter": "console"
-        }
-    },
-    "loggers": {
-        "main": {
-            "handlers": [
-                "console",
-                'file'
-            ]
-        },
-        "file": {
-            "handlers": [
-                'file'
-            ]
-        }
-    }
-}
--- cosmo_test_reporter/tests_logger/logger.py
+++ cosmo_test_reporter/tests_logger/logger.py
@@ -8,17 +8,19 @@ __author__ = 'nirb'
 
 
 def init_logger(file_name):
-    if os.path.isfile(config.LOG_DIR):
+
+    log_config = config.LoggerConfiguration(file_name)
+
+    if os.path.isfile(log_config.log_dir):
         sys.exit('file {0} exists - cloudify log directory cannot be created '
-                 'there. please remove the file and try again.'.format(config.LOG_DIR))
+                 'there. please remove the file and try again.'.format(log_config.log_dir))
 
     try:
-        config.MODULE = file_name
-        logfile = config.LOGGER['handlers']['file']['filename']
+        logfile = log_config.logger_conf['handlers']['file']['filename']
         d = os.path.dirname(logfile)
         if not os.path.exists(d):
             os.makedirs(d)
-        logging.config.dictConfig(config.LOGGER)
+        logging.config.dictConfig(log_config.logger_conf)
         lgr = logging.getLogger('main')
         lgr.setLevel(logging.INFO)
         flgr = logging.getLogger('file')
