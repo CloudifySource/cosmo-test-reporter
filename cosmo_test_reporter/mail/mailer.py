a7d04af4971ce3d5b0b3680751e756d3d51d81af
nirbir <nirb@gigaspaces.com>
2014-04-17 16:24:06 +0300

    edited report

--- cosmo_test_reporter/mail/mailer.py
+++ cosmo_test_reporter/mail/mailer.py
@@ -4,21 +4,27 @@ import smtplib
 from email.mime.text import MIMEText
 from cosmo_test_reporter import params
 
-
 def send_unit_tests_mail():
-    msg = MIMEText('There are failed unit tests')
+    message_text = """
+        <html>
+            <h1>Cosmo Unit Test Result: </h1>
+            <h2 style="color:red">There are unit test failures</h2>
+        </html>
+        """
+
+    msg = MIMEText(message_text, 'html')
     me = 'tgrid@gigaspaces.com'
-    you = 'nirb@gigaspaces.com'
+    you = ['nirb@gigaspaces.com']
 
     msg['Subject'] = 'cosmo unit tests report'
     msg['From'] = me
-    msg['To'] = you
+    msg['To'] = ', '.join(you)
 
     s = smtplib.SMTP(host=params.MAIL_HOST)
     s.ehlo()
     s.starttls()
     s.login(params.MAIL_USER, params.MAIL_PASS)
-    s.sendmail(me, [you], msg.as_string())
+    s.sendmail(me, you, msg.as_string())
     s.quit()
 
 
